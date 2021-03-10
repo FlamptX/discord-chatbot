70cfbccc67b176dd9a8aa821896f797bbe6171ac
nerdguyahmad <thisisizharahmad@gmail.com>
2021-03-10 16:13:14 +0500

    Basic Template

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import json
+from discord.ext import commands
+from prsaw import RandomStuff
+
+client= commands.Bot(command_prefix="!!", case_insensitive=True)
+re= RandomStuff()
+with open("config.json", "r") as f:
+	config= json.load(f)
+	f.close()
+
+TOKEN= config['token']
+extensions= [
+"commands",
+]
+
+
+@client.event
+async def on_ready():
+	for extension in extensions:
+		client.load_extension(f"cogs.{extension}")
+
+	if os.name == 'nt':
+		os.system("cls")
+	else:
+		os.system('clear')
+		
+	print("[ Discord ChatBot ]\n")
+	print(f"\nCurrently Logged in as: {client.user.name}")
+	print(f"ID: {client.user.id}")
